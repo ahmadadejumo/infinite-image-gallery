d0532506d0ff4048afb9841c49bca4cc64be3c88
ADE <adejumoahmad4life@gmail.com>
2021-09-15 17:16:48 +0100

    added access key in app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
 export default function App() {
+  useEffect(() => {
+    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  // returns an error if their is no access key
+  // if (!accessKey) {
+  //   return (
+  //     <a href="https://unsplash.com/developers" className="error">
+  //       Required: Get your unsplash API key first
+  //     </a>
+  //   );
+  // }
+
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
